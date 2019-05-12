61f278c7e80693a09c1bbf786724a3e24ca3139f
Hélio Andrade <hmajr.tec@gmail.com>
2019-05-12 04:11:39 -0300

    :chart_with_upwards_trend::heavy_plus_sign:Concatena dataframes bancos. Nomeia colunas

--- main.py
+++ main.py
@@ -1,46 +1,65 @@
 if __name__ == "__main__":
-    ## IMPORTA BIBLIOTECAS
+    ##
+    # IMPORTA BIBLIOTECAS
     ##
     print("CARREGANDO BIBLIOTECA...", end = " ")
     from pandas_datareader import data, wb
     import pandas as pd
     import numpy as np
     from datetime import datetime
-    print(" DONE!!!")
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    # Optional Plotly Method Imports
+    import plotly.plotly
+    import cufflinks as cf
+    cf.go_offline()
+    print(" DONE!!!\n")
     
-    # SEta tempo de busca
-    start_time = datetime(2014, 1, 1)
+
+    ##
+    # VARIABLES
+    ##
+
+    # Set search datetime
+    start_time = datetime(2010, 1, 4)
     end_time = datetime(2019, 1, 1)
+    pinrt(" == DATA UTILIZADAS ==")
+    print("Data inicio: {} \n Data fim: {}".format(start_time, end_time))
 
+    ## SERVIDOR
+    SERVER = 'stooq' #stock data source server
+    
+    ## STOCKS CODES
+    bank_dict = {"BAC": "Bank of America", "C": "City Group", "GS": "Goldman Sachs", 
+                 "JPM": "JP Morgan Chase", "MS": "Morgan Stanley", "WFC": "Wells Fargo"}
+    tickers = list(bank_dict.keys())
+    
+    df_bank = {}
+
+    ##
+    # DATA
+    ##
     print("\nCarregando dados...")
-    # FETCH - Bank of America
-    print("Bank of America...", end=" ")
-    BNK_AMR = data.DataReader("BAC", 'iex',start=start_time, end=end_time)        
-    print("OK!")
- 
-    # # FETCH - CitiGroup
-    # print("City Group...", end=" ")
-    # CTY_GRP = data.DataReader("C", 'iex',start=start_time, end=end_time)
-    # print("OK!")
+    for bank in bank_dict:
+        # FETCH - Bank
+        print("{}...".format(bank_dict[bank]), end=" ")
+        temp = data.DataReader(bank, SERVER, start=start_time, end=end_time)
+        df_bank[bank] = temp
+        print("OK!")
+    print("IMPORT DONE!!!")
 
-    # # FETCH - Goldman Sachs
-    # print("Goldman Sachs...", end=" ")
-    # GLD_SCH = data.DataReader("GD=F", 'iex',start=start_time, end=end_time)
-    # print("OK!")
+    ##
+    # DATA FRAME TREATMENT
+    ##
 
-    # # FETCH - JPMorgan Chase
-    # print("JP Morgan Chase...", end=" ")
-    # JPM_CHS = data.DataReader("JPM", 'iex',start=start_time, end=end_time)
-    # print("OK!")
+    #Concat bank stock data frame
+    bank_stocks = pd.concat(df_bank, axis=1)
 
-    # # FETCH - Morgan Stanley
-    # print("Morgan Stanley...", end=" ")
-    # MRG_STL = data.DataReader("MS", 'iex',start=start_time, end=end_time)
-    # print("OK!")
+    #Set column name level 0
+    bank_stocks.columns.names = ['Bank Ticker', 'Stock Info']
 
-    # # FETCH - Wells Fargo
-    # print("WFC...", end=" ")
-    # WLL_FRG = data.DataReader("BACWLL_FRG", 'iex',start=start_time, end=end_time)
-    # print("OK!")
+    #Check data frame
+    print(bank_stocks.head())
 
-    print("DONE!!!")
+    
